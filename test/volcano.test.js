75e43cfa570abbae433b11412f10989c700df591
Leo Bonato <leo.bonato@protonmail.com>
2021-09-03 19:32:07 +0200

    added tests

--- test/volcano.test.js
+++ test/volcano.test.js
@@ -1,13 +1,27 @@
 const Volcano = artifacts.require("VolcanoCoin");
-contract("Volcano", accounts => {
-    it("should mint 10000 VolcanoCoin", async () => {
-    const instance = await Volcano.deployed();
-    //console.log(instance);
+
+const testAddress = "0xcB7C09fEF1a308143D9bf328F2C33f33FaA46bC2";
+
+contract("Volcano", (accounts) => {
+  let instance;
+
+  beforeEach(async () => {
+    instance = await Volcano.deployed();
+  });
+
+  it("should mint 10000 VolcanoCoin", async () => {
     const totalSupply = await instance.totalSupply.call();
-    assert.equal(
-        totalSupply.toNumber(),
-       9999,
-        "Minting Failed",
-        );            
-    });
+    assert.equal(totalSupply.toNumber(), 10000, "Minting Failed");
+  });
+
+  it(`address ${testAddress} should have 0 balance`, async () => {
+    const balance = await instance.balances.call(testAddress);
+    assert.equal(balance, 0, `${testAddress} has ${balance} tokens`);
+  });
+
+  it("the owner should have all the tokens", async () => {
+    const [owner] = await web3.eth.getAccounts();
+    const balance = await instance.balances.call(owner);
+    assert.equal(balance, 10000, "owner doesn't have 10000 tokens");
+  });
 });
